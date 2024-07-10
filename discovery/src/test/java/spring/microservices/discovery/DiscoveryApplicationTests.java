826fdf4e9917dc0b705a9201bc412649fb32e980
Nanang-Wahyudi <nanang.wahyudi220503@gmail.com>
2024-07-10 19:14:02 +0700

    add: discovery (eureka) server

---
+++ discovery/src/test/java/spring/microservices/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.microservices.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
